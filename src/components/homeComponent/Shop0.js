65a90cc2711c00db1525f154877c0e5ded008e00
Mahad Ali <37039042+mahadalifazil@users.noreply.github.com>
2020-07-26 21:50:32 +0500

    Update Shop0.js

--- src/components/homeComponent/Shop0.js
+++ src/components/homeComponent/Shop0.js
@@ -5,7 +5,7 @@ import './../../components/components.css';
 export default function Shop0 () {
     return(
         <div className = "div_shop0">
-            <h2 className = "h2_shop0" >Shop Here</h2>
+            <h2 className = "h2_shop0" >Shop</h2>
             <h3 className = "h3_shop0" >Lorem Ipsum is simply dummy text of the printing and typesetting industry.</h3>
         </div>
     );
